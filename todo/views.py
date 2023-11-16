c2e971fbafd05a9e889eefb2241901b2c92d0544
mmunson94 <michaeljackmunson9994@outlook.com>
2023-11-16 16:46:33 +0000

    Add image to home page and add style to page

--- todo/views.py
+++ todo/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return render(request, 'todo_list.html')
+    return render(request, 'todo_list.html', {'name' : 'Gordon Ramsay', 'location' : 'UK'})
