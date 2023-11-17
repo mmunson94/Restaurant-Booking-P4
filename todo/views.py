0141c95d0bb71b19c4aa43607385d6a6b2859c68
mmunson94 <michaeljackmunson9994@outlook.com>
2023-11-17 16:49:30 +0000

    Set up pathway to gallery page

--- todo/views.py
+++ todo/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def say_hello(request):
     return render(request, 'todo_list.html', {'name' : 'Gordon Ramsay', 'location' : 'UK'})
+
+def gallery(request):
+    return render(request, 'gallery.html',)
