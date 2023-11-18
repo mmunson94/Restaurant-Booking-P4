73506f4ad2548db4c156914ac6d6b6f4231b43b2
mmunson94 <michaeljackmunson9994@outlook.com>
2023-11-18 16:02:09 +0000

    Work in progress: -Booking page display slots -Success page needs to be created -Possbile bug in bookings as it says $H:00(need to fix)

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import BookingDate, TimeSlot, Booking
 
 # Create your views here.
 def view_all(request):
     return  render(request, 'booking/view_all.html')
+
+def create_booking(request):
+    if request.method == 'POST':
+        timeslot_id = request.POST.get('booking_slot')
+        timeslot = TimeSlot.objects.get(id=timeslot_id)
+        Booking.objects.create(timeslot=timeslot)
+        return redirect('success_page')
+
+    dates = BookingDate.objects.all().prefetch_related('time_slots')
+    return render(request, 'booking/create.html', {'dates' : dates})
--- todo/views.py
+++ todo/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def say_hello(request):
-    return render(request, 'todo_list.html', {'name' : 'Gordon Ramsay', 'location' : 'UK'})
+# def say_hello(request):
+#     return render(request, 'todo_list.html', {'name' : 'Gordon Ramsay', 'location' : 'UK'})
 
 def gallery(request):
     return render(request, 'gallery.html',)
