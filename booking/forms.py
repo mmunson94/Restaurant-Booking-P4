d1964fdb9f5f58e36fd180c0d36a59afc99d3464
mmunson94 <michaeljackmunson9994@outlook.com>
2023-11-20 17:22:54 +0000

    Add time slot form

---
+++ booking/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import TimeSlot
+
+class TimeSlotForm(forms.ModelForm):
+    class Meta:
+        model = TimeSlot
+        fields = ['time', 'date']
+        widgets = {
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+            'date': forms.Select()
+        }
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import BookingDate, TimeSlot, Booking
+from .forms import TimeSlotForm
 
 # Create your views here.
 def view_all(request):
@@ -10,7 +11,18 @@ def create_booking(request):
         timeslot_id = request.POST.get('booking_slot')
         timeslot = TimeSlot.objects.get(id=timeslot_id)
         Booking.objects.create(timeslot=timeslot)
-        return redirect('success_page')
+        return redirect('booking_page')
 
     dates = BookingDate.objects.all().prefetch_related('time_slots')
     return render(request, 'booking/create.html', {'dates' : dates})
+
+def add_timeslot(request):
+    if request.method == 'POST':
+        form = TimeSlotForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_page')
+    else:
+        form = TimeSlotForm()
+    
+    return render(request, 'booking/add_timeslot.html', {'form': form})
